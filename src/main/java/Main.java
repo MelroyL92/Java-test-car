2996c67416e690bf1030d558ec4be9dc988733e7
MelroyL92 <melroy_lingsma@live.nl>
2023-11-24 08:13:16 +0100

    Small assignment done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,19 @@
+
+
 public class Main {
+
+    public static void main(String[] args) {
+
+
+        Car car1 = new Car("fiat", "500", "2019", "groen", "1.4" );
+        Car car2 = new Car("Toyota", "Aygo", "2016", "blauw", "1.2");
+        Car car3 = new Car ("Volvo", "CX90", "2022", "Zwart", "2.o");
+
+
+        car1.printCar();
+        car2.printCar();
+        car3.printCar();
+
+
+    }
 }
